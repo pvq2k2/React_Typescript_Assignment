a6a9a40cf2b36306c3658ed516c46d22c2545e67
pvq2k2 <vanquyet2392002e@gmail.com>
2022-03-28 22:54:25 +0700

    feat: list product with api - remove product

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 // import './App.css'
 import { Routes, Route, NavLink } from "react-router-dom";
+import { list, remove } from './api/product';
 import Dashboard from './pages/Dashboard';
 import AdminLayout from './pages/layouts/AdminLayout';
 import ProductManager from './pages/ProductManager';
-
+import { ProductType } from './types/product';
+import { Popconfirm, Modal, notification } from 'antd';
 function App() {
-
+  const [products, setProducts] = useState<ProductType[]>([]);
+  useEffect(() => {
+     const getProducts = async () => {
+       const { data } = await list();
+       setProducts(data);
+     }
+     getProducts();
+  }, []);
+  const removeItem = (id: number | string) => {
+    const openNotification = () => {
+      notification.success({
+        message: `Xóa thành công !`,
+        // description:
+        //   'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+      });
+    };
+    const confirm = window.confirm('Are you sure you want to remove this item?');
+    if (confirm) {
+      remove(id)
+      setProducts(products.filter(item => item._id !== id));
+      openNotification()
+    }
+  }
   return (
     <div className="App">
       <Routes>
@@ -16,7 +40,7 @@ function App() {
           <Route index element={<Dashboard />} />
           <Route path="dashboard" element={<Dashboard />} />
           <Route path='products'>
-            <Route index element={<ProductManager/>} />
+            <Route index element={<ProductManager products={products} onRemove={removeItem}/>} />
           </Route>
         </Route>
       </Routes>
--- src/pages/ProductManager.tsx
+++ src/pages/ProductManager.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
-import { Layout, Breadcrumb } from 'antd';
+import React, { useState } from 'react'
+import { Layout, Breadcrumb, Table, Button, Space, Modal } from 'antd';
+import { ProductType } from '../types/product';
+import { Link } from 'react-router-dom';
 
-type Props = {}
+type ProductManagerProps = {
+  products: ProductType[];
+  onRemove: (id: number | string) => void;
+}
+const ProductManager = (props: ProductManagerProps) => {
+  const columns = [
+    {
+      key: 'name',
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      key: 'price',
+      title: 'Price',
+      dataIndex: 'price',
+    },
+    {
+      key: 'action',
+      title: 'Action',
+      dataIndex: '_id',
+      render: (_id: string | number) => (
+        <Space size="middle">
+        <Button type="primary" style={{ background: '#FFCC00', color: '#000000', border: 'none'}}><Link to={`/admin/products/${_id}/edit`}>Edit</Link></Button>
+        <Button type="primary" danger onClick={() => props.onRemove(_id)}>Remove</Button>
+        </Space>
+      )
+    },
+  ]
+  const dataSource = props.products.map((item, index) => {
+    return {
+      key: index + 1,
+      name: item.name,
+      price: item.price,
+      _id: item._id
+    }
+  })
 
-const ProductManager = (props: Props) => {
   return (
     <Layout style={{ background: '#fff' }}>
     <Breadcrumb style={{ margin: '16px' }}>
@@ -11,6 +47,9 @@ const ProductManager = (props: Props) => {
     <Breadcrumb.Item>Products Management</Breadcrumb.Item>
   </Breadcrumb>
     <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+    <Button type="primary" style={{ marginBottom: 20}}><Link to={`/admin/products/add`}>Add Product</Link></Button>
+    <Table columns={columns} dataSource={dataSource} />
+                      
     </div>
     </Layout>
   )
