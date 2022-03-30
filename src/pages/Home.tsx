83899d112134e4370c1aac808025b4f439ef83d1
pvq2k2 <vanquyet2392002e@gmail.com>
2022-03-30 20:41:26 +0700

    feat: Nav componend

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Header from '../components/header';
+import Nav from '../components/Nav';
 type Props = {}
 
 const Home = (props: Props) => {
   return (
+    <div>
       <Header />
+      <Nav />
+    </div>
+      
   )
 }
 
